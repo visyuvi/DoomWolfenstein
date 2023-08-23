224095fc3ad8f63d546d6c2e1a5bdc50cbceeeaf
Your Name <you@example.com>
2023-08-23 10:33:55 +0530

    Player can only hit npc if it in line of sight. Play death animation and death sound when npc's health < 1

--- sound.py
+++ sound.py
@@ -10,6 +10,7 @@ class Sound:
         self.npc_pain = pg.mixer.Sound(self.path + 'npc_pain.wav')
         self.npc_pain.set_volume(0.35)
         self.npc_death = pg.mixer.Sound(self.path + 'npc_death.wav')
+        self.npc_death.set_volume(0.35)
         self.npc_shot = pg.mixer.Sound(self.path + 'npc_attack.wav')
         self.player_pain = pg.mixer.Sound(self.path + 'player_pain.wav')
         self.theme = pg.mixer.music.load(self.path + 'theme.mp3')
